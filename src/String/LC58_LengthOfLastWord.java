d9e655844089ecf72a1e349569a0192c9301ca26
unknown <cccccynthia7@outlook.com>
2016-07-11 19:21:33 +0800

    add Length Of Last Word

---
+++ src/String/LC58_LengthOfLastWord.java
@@ -0,0 +1,25 @@
+package String;
+/**
+ * https://leetcode.com/problems/length-of-last-word/
+ * 
+ * Tag: String (Easy)
+ * 
+ * 输出空格后的最后一串字符串的长度
+ * 就注意下前后有空格，没有空格的情况
+ * 
+ * 时间复杂度:O(n), 1ms, beats 44.23%
+ * 用的函数，没什么可说的
+ * 
+ */
+public class LC58_LengthOfLastWord {
+	public int lengthOfLastWord(String s) {
+        int i = s.trim().lastIndexOf(" ");
+        int len = s.trim().length();
+        if (i < 0){
+            return len;
+        }
+        else{
+            return (len - i - 1);
+        }
+    }
+}
